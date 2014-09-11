0085521a993ecc56d93ea201d8b31964c9fdb5d0
xu xiaolong <bupt.xxl.qinfen@gmail.com>
2014-09-11 16:47:47 +0800

    some strcpy function test

--- SelfPrec/myStrcpy.cpp
+++ SelfPrec/myStrcpy.cpp
@@ -1,58 +1,33 @@
-/*
-  This is for atoi
-  First is from linux c
-  second is mine
-  note: using const to protect the source value
-*/
 #include <iostream>
 using namespace std;
-long __decl_atol(const char *nptr);
-int __decl_atoi(const char *nptr);
-int myAtoi(const char *nptr);
+void strcpy_0(char *to,char *from);
+void strcpy_1(char *to,char *from);
+void strcpy_2(char *to,char *from);
+void strcpy_3(char *to,char *from);
 int main(){
-    cout << "Input the str:\n";
-    char str[300];
-    cin>>str;
-    cout << "value is :" << myAtoi(str) <<endl;
+    char from[100];
+    cin >> from;
+    char toChr = 't';
+    char *to = &toChr;
+    char *print = to;
+    strcpy_0(to,from);
+    cout << print <<endl;
     return 0;
 }
-long __decl_atol(const char *nptr){
-    int c,sign;
-    long total=0;
-    while( isspace( (int)(unsigned char)*nptr ) ) nptr ++;
-    sign = c = (int) (unsigned char) *nptr++;
-    if( c == '+' || c == '-')
-        c = (int)(unsigned char) *nptr++;
-    while(isdigit(c)){
-        total = total * 10 + (c - '0');
-        c = (int) ( unsigned char) *nptr++;
-    }
-    if(sign == '-')
-        return - total;
-    return total;
+void strcpy_0(char *to ,char *from){
+    while( (*to++ = *from++) != '\0');
 }
-int __decl_atoi(const char *nptr){
-    return (int)__decl_atol(nptr);
+void strcpy_1(char *to ,const char *from){
+    while( (*to ++ = *from ++) != '\0' );
 }
-//考虑情况
-/*
- 1，前面有空
- 2，非法字符
- 3，单符号问题
- 4，溢出
- */
-int myAtoi(const char * str){
-    if( str == NULL|| strlen(str)<1) return 0;
-    while(*str == ' ') str++;
-    int c,sign;
-    long total=0;
-    sign = c = *str++;
-    if(c=='-' || c == '+') c = *str++;
-    while(c>='0' && c<= '9'){
-        total = total * 10 + c - '0';
-        c = *str++;
-    }
-    if(sign == '-') return (int)(-total);
-    return (int)total;
+void strcpy_2(char *to, const char *from){
+    if(from == NULL)
+        return;
+    while( (*to ++ = *from ++) != '\0' );
 }
-
+char * strcpy_3(char *to ,const char * from){
+    if(from == NULL) return NULL;
+    char * result = to;
+    while( (*to ++ = *from ++) != '\0' );
+    return result; 
+}
